1886f2967055eb513a79972e4a90e7745e580d93
franciscosucre <franciscojsucreg@gmail.com>
2018-11-04 13:43:13 +0100

    Updates

---
+++ precache-manifest.0627dc1d681a8425f0edb3c70455c759.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5eeb900dfd5133dee24b",
+    "url": "/static/js/main.5eeb900d.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "5eeb900dfd5133dee24b",
+    "url": "/static/css/main.0af4e1d2.chunk.css"
+  },
+  {
+    "revision": "49fb5be4c28ba13f2b040b947a3d3553",
+    "url": "/index.html"
+  }
+];
